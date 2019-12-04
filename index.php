8dd88669c4fda1d2c6f7567240cfe257818b62aa
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-04 10:37:47 +0100

    PHP3Ex4

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// On crée notre array $coordonnees
+$department = array (
+'02' => 'Aisne',
+'80' => 'Somme',
+'60' => 'Oise',
+'59' => 'Nord',
+'62' => 'Pas-de-Calais');
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>PHP3Ex4</title>
+  </head>
+  <body>
+  </body>
+</html>
